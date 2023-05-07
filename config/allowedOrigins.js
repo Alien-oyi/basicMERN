68dc235685c919b105b4d9331655b02211d183cd
Alien-oyi <daijinmin78@gmail.com>
2023-05-07 13:56:11 -0400

    deploy

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,5 @@
 const allowedOrigins = [
-    'http://localhost:3001',
-    'http://localhost:3000',
+    https://technotes-a5sz.onrender.com
 ]
 
 module.exports = allowedOrigins;
--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -5,7 +5,7 @@ const errorHandle = (err, req, res, next) => {
     console.log(err.stack)
     const status = res.statusCode ? res.statusCode : 500
     res.status(status)
-    res.json({ message: err.message })
+    res.json({ message: err.message ,isError: true})
 }
 
 module.exports =  errorHandle 
